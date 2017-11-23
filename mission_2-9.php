037e80ad5e08e1f89d5e3c5aba419777fd362731
Kokisi4e173 <33916823+Kokisi4e173@users.noreply.github.com>
2017-11-23 11:10:15 +0900

    Update mission_2-9.php

--- mission_2-9.php
+++ mission_2-9.php
@@ -1,5 +1,5 @@
 <?php
-$pdo=new PDO('mysql:host=localhost;dbname=co_932_it_99sv_coco_com','co-932.it.99sv-c','8By6cYds');
+$pdo=new PDO('データベース名’’ユーザー名’’パスワード’);
 $sql='SHOW TABLES';
 $result=$pdo->query($sql);
 foreach($result as $row){
