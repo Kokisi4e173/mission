12658755958ae05565ce333f22b21cc78526cda2
Kokisi4e173 <33916823+Kokisi4e173@users.noreply.github.com>
2017-11-23 11:21:17 +0900

    Update mission_3-signup.php

--- mission_3-signup.php
+++ mission_3-signup.php
@@ -1,53 +1,53 @@
 <?php
 
-// �Z�b�V�����J�n
+// セッション開始
 session_start();
 
-$db['host'] = "localhost";  // DB�T�[�o��URL
-$db['user'] = "hogeUser";  // ���[�U�[��
-$db['pass'] = "hogehoge";  // ���[�U�[���̃p�X���[�h
-$db['dbname'] = "loginManagement";  // �f�[�^�x�[�X��
+$db['host'] = "DBサーバのURL";  // DBサーバのURL
+$db['user'] = "ユーザー名";  // ユーザー名
+$db['pass'] = "ユーザー名のパスワード";  // ユーザー名のパスワード
+$db['dbname'] = "データベース名";  // データベース名
 
-// �G���[���b�Z�[�W�A�o�^�������b�Z�[�W�̏�����
+// エラーメッセージ、登録完了メッセージの初期化
 $errorMessage = "";
 $signUpMessage = "";
 
-// ���O�C���{�^���������ꂽ�ꍇ
+// ログインボタンが押された場合
 if (isset($_POST["signUp"])) {
-    // 1. ���[�UID�̓��̓`�F�b�N
-    if (empty($_POST["username"])) {  // �l����̂Ƃ�
-        $errorMessage = '���[�U�[ID�������͂ł��B';
+    // 1. ユーザIDの入力チェック
+    if (empty($_POST["username"])) {  // 値が空のとき
+        $errorMessage = 'ユーザーIDが未入力です。';
     } else if (empty($_POST["password"])) {
-        $errorMessage = '�p�X���[�h�������͂ł��B';
+        $errorMessage = 'パスワードが未入力です。';
     } else if (empty($_POST["password2"])) {
-        $errorMessage = '�p�X���[�h�������͂ł��B';
+        $errorMessage = 'パスワードが未入力です。';
     }
 
     if (!empty($_POST["username"]) && !empty($_POST["password"]) && !empty($_POST["password2"]) && $_POST["password"] === $_POST["password2"]) {
-        // ���͂������[�UID�ƃp�X���[�h���i�[
+        // 入力したユーザIDとパスワードを格納
         $username = $_POST["username"];
         $password = $_POST["password"];
 
-        // 2. ���[�UID�ƃp�X���[�h�����͂���Ă�����F�؂���
-        $dsn = sprintf('mysql: host=%s; dbname=%s; charset=utf8', $db['host'], $db['dbname']);
+        // 2. ユーザIDとパスワードが入力されていたら認証する
+        $dsn = sprintf('mysql: host=ホスト名; dbname=データベース名; charset=utf8', $db['ホスト名'], $db['データベース名']);
 
-        // 3. �G���[����
+        // 3. エラー処理
         try {
             $pdo = new PDO($dsn, $db['user'], $db['pass'], array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
 
             $stmt = $pdo->prepare("INSERT INTO userData(name, password) VALUES (?, ?)");
 
-            $stmt->execute(array($username, password_hash($password, PASSWORD_DEFAULT)));  // �p�X���[�h�̃n�b�V�������s���i����͕�����݂̂Ȃ̂�bindValue(�ϐ��̓��e���ς��Ȃ�)���g�p�����A����excute�ɓn���Ă����Ȃ��j
-            $userid = $pdo->lastinsertid();  // �o�^����(DB����auto_increment����)ID��$userid�ɓ����
+            $stmt->execute(array($username, password_hash($password, PASSWORD_DEFAULT)));  // パスワードのハッシュ化を行う（今回は文字列のみなのでbindValue(変数の内容が変わらない)を使用せず、直接excuteに渡しても問題ない）
+            $userid = $pdo->lastinsertid();  // 登録した(DB側でauto_incrementした)IDを$useridに入れる
 
-            $signUpMessage = '�o�^���������܂����B���Ȃ��̓o�^ID�� '. $userid. ' �ł��B�p�X���[�h�� '. $password. ' �ł��B';  // ���O�C�����Ɏg�p����ID�ƃp�X���[�h
+            $signUpMessage = '登録が完了しました。あなたの登録IDは '. $userid. ' です。パスワードは '. $password. ' です。';  // ログイン時に使用するIDとパスワード
         } catch (PDOException $e) {
-            $errorMessage = '�f�[�^�x�[�X�G���[';
-            // $e->getMessage() �ŃG���[���e���Q�Ɖ\�i�f�o�b�N���̂ݕ\���j
+            $errorMessage = 'データベースエラー';
+            // $e->getMessage() でエラー内容を参照可能（デバック時のみ表示）
             // echo $e->getMessage();
         }
     } else if($_POST["password"] != $_POST["password2"]) {
-        $errorMessage = '�p�X���[�h�Ɍ�肪����܂��B';
+        $errorMessage = 'パスワードに誤りがあります。';
     }
 }
 ?>
@@ -56,27 +56,27 @@ if (isset($_POST["signUp"])) {
 <html>
     <head>
           
-            <title>�V�K�o�^</title>
+            <title>新規登録</title>
     </head>
     <body>
-        <h1>�V�K�o�^���</h1>
+        <h1>新規登録画面</h1>
         <form id="loginForm" name="loginForm" action="" method="POST">
             <fieldset>
-                <legend>�V�K�o�^�t�H�[��</legend>
+                <legend>新規登録フォーム</legend>
                 <div><font color="#ff0000"><?php echo htmlspecialchars($errorMessage, ENT_QUOTES); ?></font></div>
                 <div><font color="#0000ff"><?php echo htmlspecialchars($signUpMessage, ENT_QUOTES); ?></font></div>
-                <label for="username">���[�U�[��</label><input type="text" id="username" name="username" placeholder="���[�U�[�������" value="<?php if (!empty($_POST["username"])) {echo htmlspecialchars($_POST["username"], ENT_QUOTES);} ?>">
+                <label for="username">ユーザー名</label><input type="text" id="username" name="username" placeholder="ユーザー名を入力" value="<?php if (!empty($_POST["username"])) {echo htmlspecialchars($_POST["username"], ENT_QUOTES);} ?>">
                 <br>
-                <label for="password">�p�X���[�h</label><input type="password" id="password" name="password" value="" placeholder="�p�X���[�h�����">
+                <label for="password">パスワード</label><input type="password" id="password" name="password" value="" placeholder="パスワードを入力">
                 <br>
-                <label for="password2">�p�X���[�h(�m�F�p)</label><input type="password" id="password2" name="password2" value="" placeholder="�ēx�p�X���[�h�����">
+                <label for="password2">パスワード(確認用)</label><input type="password" id="password2" name="password2" value="" placeholder="再度パスワードを入力">
                 <br>
-                <input type="submit" id="signUp" name="signUp" value="�V�K�o�^">
+                <input type="submit" id="signUp" name="signUp" value="新規登録">
             </fieldset>
         </form>
         <br>
         <form action="mission_3-login.php">
-            <input type="submit" value="�߂�">
+            <input type="submit" value="戻る">
         </form>
     </body>
 </html>
