3bf37a17853b3bc670238fa7c34375b43eac0f21
Kokisi4e173 <33916823+Kokisi4e173@users.noreply.github.com>
2017-11-23 11:10:50 +0900

    Update mission_2-11.php

--- mission_2-11.php
+++ mission_2-11.php
@@ -1,15 +1,15 @@
 <?php
 
-$pdo=new PDO('mysql:host=localhost;dbname=co_932_it_99sv_coco_com','co-932.it.99sv-c','8By6cYds');
+$pdo=new PDO('データベース名’’ユーザー名’’パスワード’);
 
-// INSERT����ϐ��Ɋi�[
+// INSERT文を変数に格納
 $sql = "INSERT INTO menu (name, population, created) VALUES (:name, :population, now())";
 
 $result=$pdo->query($sql);
  
  
-// �o�^�����̃��b�Z�[�W
-echo '�o�^�������܂���';
+// 登録完了のメッセージ
+echo '登録完了しました';
 
 
 
