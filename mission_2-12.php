4aa7ea2db462c8a00f0f1b8ae6fde54303e5641b
Kokisi4e173 <33916823+Kokisi4e173@users.noreply.github.com>
2017-11-23 11:11:07 +0900

    Update mission_2-12.php

--- mission_2-12.php
+++ mission_2-12.php
@@ -1,20 +1,20 @@
 <?php
 
-$pdo=new PDO('mysql:host=localhost;dbname=co_932_it_99sv_coco_com','co-932.it.99sv-c','8By6cYds');
+$pdo=new PDO('データベース名’’ユーザー名’’パスワード’);
 
 
 
 
-// SELECT����ϐ��Ɋi�[
+// SELECT文を変数に格納
 $sql = "SELECT * FROM menu";
  
 $result = $pdo->query($sql);
  
-// foreach���Ŕz��̒��g����s���o��
+// foreach文で配列の中身を一行ずつ出力
 foreach ($result as $row) {
  
-  // �f�[�^�x�[�X�̃t�B�[���h���ŏo��
-echo $row['name'].'�F';
+  // データベースのフィールド名で出力
+echo $row['name'].'：';
 echo $row['text'].'<br>';
  
 }
