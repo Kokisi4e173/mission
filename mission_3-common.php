f9829e4d0aae061d2fd618009c9b1973339ed60b
Kokisi4e173 <33916823+Kokisi4e173@users.noreply.github.com>
2017-11-23 11:16:20 +0900

    Update mission_3-common.php

--- mission_3-common.php
+++ mission_3-common.php
@@ -15,7 +15,7 @@ function connect_db()
     $password = 'password';
    
    
-    return new PDO('mysql:host=localhost;dbname=co_932_it_99sv_coco_com','co-932.it.99sv-c','8By6cYds');
+    return new PDO('mysql:host=ホスト名;dbname=データベース名','ユーザー名','パスワード');
 }
 
 /**
